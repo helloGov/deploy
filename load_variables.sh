6d837e2c332b2e7f5b28763058983b3fe6e704fb
Tim Gupta <timgupta@tim-mac.attlocal.net>
2017-04-29 00:09:04 -0700

    Change "key" to "keyfile" in docs.

--- load_variables.sh
+++ load_variables.sh
@@ -1,5 +1,5 @@
 # default values
-SERVER=104.199.132.136 
+SERVER=104.199.132.136
 APP_DIR=/var/www/hellogov
 ARCHIVE_DIR=/home/hellogov/archive
 GIT_URL="git://github.com/helloGov/singleAction"
@@ -7,8 +7,8 @@ REMOTE_SCRIPT_PATH=/tmp
 KEY_FILE_PATH=~/.ssh/gc
 
 function usage {
-	echo "Usage: $0 -server=\$SERVER_IP_ADDRESS -key=\$PATH_TO_KEY_FILE"
-	echo "Defaults to server: $SERVER, key: $KEY_FILE_PATH"
+	echo "Usage: $0 -server=\$SERVER_IP_ADDRESS -keyfile=\$PATH_TO_KEY_FILE"
+	echo "Defaults to server: $SERVER, keyfile: $KEY_FILE_PATH"
 }
 
 #parse args
@@ -17,14 +17,14 @@ do
 case $i in
     -server=*|--server=*|-s=*)
     SERVER="${i#*=}"
-    shift 
+    shift
     ;;
     -keyfile=*|--keyfile=*)
     KEY_FILE_PATH="${i#*=}"
-    shift 
+    shift
     ;;
     *)
-    usage 
+    usage
     ;;
 esac
 done
